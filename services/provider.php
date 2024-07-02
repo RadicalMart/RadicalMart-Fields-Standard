14c453c97023985a132cab48f11e269cf7a85057
Igor Berdichevskiy <septdir@gmail.com>
2024-07-02 13:47:54 +0300

    Reset to dev

--- services/provider.php
+++ services/provider.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Fields Standard Plugin
  * @subpackage  plg_radicalmart_fields_standard
- * @version     1.2.5
+ * @version     __DEPLOY_VERSION__
  * @author      RadicalMart Team - radicalmart.ru
  * @copyright   Copyright (c) 2024 RadicalMart. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
